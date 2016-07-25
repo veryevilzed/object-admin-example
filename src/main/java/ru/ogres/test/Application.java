f1597e85ccc7f7ef85432a0ecbbcb4c0663593ea
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2016-07-25 21:28:02 +0300

    test config anotation

--- src/main/java/ru/ogres/test/Application.java
+++ src/main/java/ru/ogres/test/Application.java
@@ -4,15 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.ogres.test.db.User;
 import ru.ogres.test.db.UserRepo;
+import ru.ogres.tools.EnableObjectAdmin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zed on 22.07.16.
  */
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = {"ru.ogres.tools.repo", "ru.ogres.test"})
+@EnableObjectAdmin
+@ComponentScan(basePackages = {"ru.ogres.tools.repo"})
 public class Application implements CommandLineRunner {
 
     @Autowired
@@ -21,6 +28,11 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         User u = new User();
+        u.setName("Zed");
+        u.setAccess(5);
+        userRepo.save(u);
+
+
     }
 
     public static void main(final String[] args) throws Exception {
